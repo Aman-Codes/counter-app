12a8b19aa816299f43d2d64491a31b35eb5572a0
Aman Dwivedi <aman.dwivedi5@gmail.com>
2020-07-31 09:42:52 +0530

    Updates

---
+++ precache-manifest.3abae32ebeeabb6e2515f5c5142cfa1d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f634e30435b435aff83",
+    "url": "/counter-app/static/css/main.ca93e64b.chunk.css"
+  },
+  {
+    "revision": "3f634e30435b435aff83",
+    "url": "/counter-app/static/js/main.c48537e9.chunk.js"
+  },
+  {
+    "revision": "0f1f037b32656fd93884",
+    "url": "/counter-app/static/js/runtime~main.c63f0888.js"
+  },
+  {
+    "revision": "76dc1d4c276a2199378a",
+    "url": "/counter-app/static/css/2.422f46a8.chunk.css"
+  },
+  {
+    "revision": "76dc1d4c276a2199378a",
+    "url": "/counter-app/static/js/2.cdc130cc.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/counter-app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/counter-app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/counter-app/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/counter-app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/counter-app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "581bb25bbcaca3317cda5225f23e8f79",
+    "url": "/counter-app/index.html"
+  }
+];
